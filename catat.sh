79589767d7b09f765a1d80daa06ce2e3bd8cb11b
Sugeng Wahyudi <82944512+Gengsu07@users.noreply.github.com>
2024-01-20 15:33:56 +0000

    done:ingesting data using scripts in docker container

--- catat.sh
+++ catat.sh
@@ -1,20 +1,23 @@
 python ingest_data.py \
     --user=gengsu07\
-    --password=Gengsu!sh3r3\
+    --password=sgwi2341\
     --host=localhost\
     --port=5433\
     --db=ny_taxi\
     --table=ingest_taxi_data\
     --url=https://raw.githubusercontent.com/Gengsu07/Gengsu07_DEZoomCamp/main/1.containerization/dockerizing_ingest_script/nyc_taxi_green.csv
 
+
+
+
 docker run -it \
-   --network=postgres_pgadmin_pg-network \
-     ingest_data:latest \ 
-     python ingest_data.py \
-    --user=gengsu07 \
-    --password=sgwi2341 \
-    --host=172.23.0.3 \
-    --port=5432 \
-    --db=ny_taxi \
-    --table=taxi_data \
-    --url=https://raw.githubusercontent.com/Gengsu07/Gengsu07_DEZoomCamp/main/1.containerization/dockerizing_ingest_script/nyc_taxi_green.csv
+  --network=postgres_pgadmin_pg-network \
+  ingest:latest \
+  --user=gengsu07 \
+  --password=sgwi2341 \
+  --host=postgres_pgadmin-postgresDB-1 \
+  --db=ny_taxi \
+  --table=taxi_data \
+  --port=5432 \
+  --url=https://raw.githubusercontent.com/Gengsu07/Gengsu07_DEZoomCamp/main/1.containerization/dockerizing_ingest_script/nyc_taxi_green.csv
+
