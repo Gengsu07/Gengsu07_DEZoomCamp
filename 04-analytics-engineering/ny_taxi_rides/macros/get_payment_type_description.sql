d9571afd6684beafd4229917dd0295923b92c490
Sugeng Wahyudi <sugengdatascience@gmail.com>
2024-02-22 19:45:21 +0000

    add nyc_taxi_dbtproject

---
+++ 04-analytics-engineering/ny_taxi_rides/macros/get_payment_type_description.sql
@@ -0,0 +1,18 @@
+{# 
+ini macros untuk membuat field tipe pembayaran 
+#}
+
+{% macro get_payment_type_description(payment_type) -%}
+
+case {{ dbt.safe_cast("payment_type", api.Column.translate_type("integer")) }}  
+   when 1 then 'Credit card'
+        when 2 then 'Cash'
+        when 3 then 'No charge'
+        when 4 then 'Dispute'
+        when 5 then 'Unknown'
+        when 6 then 'Voided trip'
+        else 'EMPTY'
+    end
+
+{%- endmacro %}
+
